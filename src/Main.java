ba184c8f382341fa5d45bca58ed796c4bbd3f514
Renanzin <joserenan.bsf@gmail.com>
2025-09-08 20:52:14 -0300

    Função de somar de dois números
    Foi criada uma função de somar de dois números

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 public class Main {
+    public static int somar(int num1, int num2){
+        return num1 + num2;
+    }
     public static void main(String[] args) {
         System.out.print("Olá, Github!");
     }
