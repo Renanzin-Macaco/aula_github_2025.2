7302ca35c92fc402fa79950bdb8c1df5052057d4
Renanzin <joserenan.bsf@gmail.com>
2025-09-08 21:34:55 -0300

    Meu primeiro objeto
    Eu criei meu primeiro objeto

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
-    public static int somar(int num1, int num2){
-        return num1 + num2;
-    }
     public static void main(String[] args) {
-        System.out.print("Olá, Github!");
-        int result = somar(5,6);
-        System.out.println(result);
+            Pessoa pessoal = new Pessoa();
+            pessoal.nome = "Renan";
+            pessoal.idade = 17;
+            System.out.println(pessoal.nome);
+            System.out.println(pessoal.idade);
     }
 }
